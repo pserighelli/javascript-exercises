1eb3c7d894429f0a909a242ba1f284a24b430565
pserighelli <155854304+pserighelli@users.noreply.github.com>
2024-09-10 22:20:37 +0200

    Solves exercise #4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+
+    let uniqueArray = [...new Set(array)];
+    
+    for (const remove of args) {
+
+        if (uniqueArray.includes(remove) == false)
+            continue
+
+        let index = uniqueArray.indexOf(remove);
+        uniqueArray.splice(index, 1);
+
+        }
+
+    return uniqueArray;
+
 };
 
 // Do not edit below this line
