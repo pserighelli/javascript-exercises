4eb7295ff9976a10cfcab250182c2bc91c73803a
pserighelli <155854304+pserighelli@users.noreply.github.com>
2024-09-14 14:17:49 +0200

    Solves exercise #5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+
+    let sum = 0;
+
+    if (firstNum > lastNum) {
+        let array = [firstNum, lastNum];
+        firstNum = array[1];
+        lastNum = array[0];
+    }
+
+    if (firstNum < 0 || typeof firstNum !== 'number' || Number.isSafeInteger(firstNum) === false || lastNum < 0 || typeof lastNum !== 'number' || Number.isSafeInteger(firstNum) === false) {
+        return 'ERROR';
+    }
+
+    for (let i = 0; i <= (lastNum - firstNum); i++) {
+        sum = sum + (firstNum + i);
+    }
+
+return sum;
 
 };
 
