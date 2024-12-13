3c38fca3222f6c435d485927bb3cbd8f93cac028
pserighelli <155854304+pserighelli@users.noreply.github.com>
2024-12-13 15:14:05 +0100

    Solves Javascript exercises 08 to 12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+
+    if(index < 0) {
+        return "OOPS";
+    }
+
+    if(typeof index !== "number") {
+        index = parseInt(index);
+    }
+
+    let sequence = [0, 1];
+
+    for (let i = 2; i <= index; i++) {
+        sequence[i] = sequence[i-1] + sequence[i-2];
+    }
+
+    return sequence[index];
+
 
 };
 
